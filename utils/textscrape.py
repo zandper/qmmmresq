b9181132337a064f8e5595ec048764a8f76705cd
zandper <z.peraza2013@gmail.com>
2025-08-29 10:27:35 -0400

    cleaned up scripts and added shebang

--- utils/textscrape.py
+++ utils/textscrape.py
@@ -1,5 +1,11 @@
 import re
 
+def get_molids(in_path):
+    #get molid of molecules in qmm region
+    with open(in_path, 'r') as file:
+        molid_list = [int(m) for m in re.findall(r'\bmolid\s*(\d+)', file.read())]
+    return molid_list
+
 def extract_first_wavelength(file_path):
     with open(file_path, 'r') as file:
         content = file.read()
