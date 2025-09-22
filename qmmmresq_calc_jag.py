3ea5af0f3c7720563fd3f89dd29d790ac3ab1cd1
zandper <z.peraza2013@gmail.com>
2025-09-22 13:35:54 -0400

    added jag

---
+++ qmmmresq_calc_jag.py
@@ -0,0 +1,194 @@
+#!/usr/bin/env python3
+from schrodinger import structure
+from schrodinger.application.qsite import output
+#from schrodinger.test import mmshare_data_fil
+
+import os
+import re
+import shutil
+import subprocess
+import multiprocessing
+import argparse
+from utils import textscrape
+from schrodinger.job.jobcontrol import launch_job
+from schrodinger.application.jaguar.output import JaguarOutput
+
+def calc_single_point_residue(res, mol, mae_st, r_dir, mae_fname, in_path, p_dir):
+    copy_st = mae_st.copy() # Make copy of original structure 
+    atom_list = res._atoms # Get index of all atoms in residue
+    for i in atom_list: # Set partial charge of all atoms in residue to 0 
+        copy_st.atom[i].partial_charge = 0
+        copy_st.atom[i].solvation_charge = 0 #ASK ABOUT THE SOLVATION CHARGE
+
+    res_num_str = str(res.resnum).zfill(5)
+    print(res_num_str)
+    res_dir=os.path.join(r_dir,res_num_str)
+    os.makedirs(res_dir,exist_ok=True)
+    mae_copy_path = os.path.join(res_dir,mae_fname)
+    in_copy_path = mae_copy_path.replace(".mae",".in")
+    shutil.copyfile(in_path, in_copy_path)
+    with structure.StructureWriter(mae_copy_path) as writer: #write modified .mae file
+        writer.append(copy_st)
+    cmd_dir = os.path.realpath(res_dir)
+    p = subprocess.Popen(['qsite', '-WAIT','-HOST','localhost', os.path.basename(in_copy_path)], cwd=cmd_dir) #run qsite calculation
+    p.wait()
+    out_path = mae_copy_path.replace(".mae",".out")
+    result = output.QSiteOutput(out_path)
+    wavelength_nm = textscrape.extract_first_wavelength(out_path)
+    print(f"Total_E: {result.energy}")
+    print(f"Wavelength: {wavelength_nm}")
+
+
+    with open(os.path.join(p_dir,f"{res_num_str}.txt"),"w") as f:
+        f.write(f"{res_num_str}\t{result.energy}\t{wavelength_nm}")
+    #shutil.rmtree(res_dir) 
+
+
+def process_all_residues(mol, mae_st, r_dir, mae_fname, in_path, p_dir):
+    # Use multiprocessing Pool to parallelize the work
+    with multiprocessing.Pool(processes=num_processes) as pool:
+        # Pass all arguments required for each residue to the pool
+        args = [(res, mol, mae_st, r_dir, mae_fname, in_path, p_dir) for res in mol.residue]
+        pool.starmap(calc_single_point_residue, args)
+
+def chunk_list(input_list, chunk_size):
+    """Yield successive chunks of size `chunk_size` from `input_list`."""
+    for i in range(0, len(input_list), chunk_size):
+        yield input_list[i:i + chunk_size]
+
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description="Process MAE and input files")
+
+    #parser.add_argument('-m', '--mae_path', type=str, required=True, help='Path to .mae file')
+    parser.add_argument('-i', '--in_path', type=str, required=True, help='Path to .in file')
+    parser.add_argument('-p', '--nproc', type=int, default=12, help='Number of CPU processes to use (default: 12)')
+    args = parser.parse_args()
+
+    in_path = os.path.realpath(args.in_path)
+    mae_path = in_path.replace(".in",".mae")
+    num_processes=12
+
+    mae_fname=os.path.basename(mae_path)
+    
+    molid_list = textscrape.get_molids(in_path)
+
+    print(molid_list)
+
+    mae_st = structure.StructureReader.read(mae_path)
+
+    p_dir="parameters"
+    os.makedirs(p_dir, exist_ok=True)
+    r_dir="residues"
+    os.makedirs(r_dir, exist_ok=True)
+
+
+    #iterate through all molecules in structure
+    for mol in mae_st.molecule:
+        #Skip molecules which are in qmm region
+        if mol.number in molid_list:
+            print(f"molecule #{mol.number} in qmm region, skipping molecule")
+        else:
+
+            #Asynchronously calculate for all residues in a molecule
+           #process_all_residues(mol, mae_st, r_dir, mae_fname, in_path, p_dir)
+            
+            residues = list(mol.residue)
+            res_batches = list(chunk_list(residues, args.nproc))  # Break residues into batches of size n_cpus
+
+            for batch_num, res_batch in enumerate(res_batches):
+                job_inputs = []  # Collect input files for this batch
+
+                for res in res_batch:
+                    copy_st = mae_st.copy()
+                    atom_list = res._atoms
+
+                    for i in atom_list:
+                        copy_st.atom[i].partial_charge = 0
+                        copy_st.atom[i].solvation_charge = 0
+
+                    res_num_str = str(res.resnum).zfill(5)
+                    #res_dir = os.path.join(r_dir, res_num_str)
+                    #os.makedirs(res_dir, exist_ok=True)
+
+                    mae_copy_path = os.path.join(r_dir, f'{res_num_str}.mae')
+                    in_copy_path = mae_copy_path.replace(".mae", ".in")
+
+                    with open(in_path, 'r') as f:
+                        content = f.read().replace(os.path.basename(mae_path),os.path.basename(mae_copy_path))
+
+                    with open(in_copy_path, 'w') as f:
+                        f.write(content)
+                    
+                    with structure.StructureWriter(mae_copy_path) as writer:
+                        writer.append(copy_st)
+
+                    job_inputs.append(os.path.basename(in_copy_path))  # Collect .in files for this batch
+                
+                os.chdir(r_dir)
+                # Launch the batch as one job (can be modified to parallelize)
+                run_command = ["jaguar", "run"] + job_inputs + ["-jobname", f"batch_{batch_num}", "-HOST", "localhost",'-PARALLEL',str(args.nproc)]
+                print(run_command)
+                job = launch_job(run_command)#,nproc=args.nproc+1,proj=r_dir)
+                job.wait()
+                os.chdir('..')
+                for in_file in job_inputs:
+                    in_path = os.path.join(r_dir,in_file)
+                    out_path = in_path.replace(".in", ".out")
+                    result = output.QSiteOutput(out_path)
+                    wavelength_nm = textscrape.extract_first_wavelength(out_path)
+                    print(f"Total_E: {result.energy}")
+                    print(f"Wavelength: {wavelength_nm}")
+                    with open(os.path.join(p_dir,f"{res_num_str}.txt"),"w") as f:
+                        f.write(f"{res_num_str}\t{result.energy}\t{wavelength_nm}")
+
+        #DO THIS PART ASYNCHRONOUSLY
+
+        #t = structure._AtomCollection(mae_st, atom_list)
+        """
+        print(t[0])
+        exit()
+        st_temp = mae_st.copy()
+        st_temp.property = st_temp.property
+        print(st_temp.property)
+        atom_list= res._atoms
+        print(mae_st.atom[atom_list])
+        """
+        #nres = build.neutralize_structure(res)
+        #print(dir(atoms))
+        #print(res.getCode())
+        #print(res.resnum) #resnum is the mol
+        #atom_list= res._atoms
+        #set atom charges to 0
+        #print(atom_list)
+        #mae_st()
+        #set charge of residue equal to 0
+        
+        #save .mae file
+        #save .in file (needs to have matching name)
+        #run calc
+
+        #print(dir(res))
+
+        #temp_mae_st = mae_st.neutralize residue
+
+            
+
+#USE THIS FOR MEASURING PROXIMITY TO CHROMOPHORE
+#schrodinger.structutils.measure.get_shortest_distance(st, atoms=None, st2=None, cutoff=inf)
+
+
+"""
+residues = residues not in qm region x
+for i in residues: x
+    set residue to neutral charge x
+    calc single point energy
+    write energy
+    chromophore distance
+"""
+
+"""
+plot delta energy vs residue (sort by chromophore distance)
+"""
